191591389c9a2b3e20b7ccbfa1eb9ca9755d26a2
UseApple918 <136140544+UseApple918@users.noreply.github.com>
2024-01-30 13:55:03 -0500

    Guardar commit

--- Sistema_Ventana.py
+++ Sistema_Ventana.py
@@ -1,6 +1,16 @@
 import sys
+from typing import Optional
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QWidget , QPushButton , QLabel , QStyle
+from PySide6.QtWidgets import QApplication, QWidget , QPushButton , QLabel, QVBoxLayout
+global ventana
+class Main_Label(QLabel):
+    def __init__(self, texto):
+        super().__init__(texto)
+
+class Posiciones (QVBoxLayout):
+    def __init__(self, Label):
+        super().__init__(Label)
+          
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -8,13 +18,15 @@ class MainWindow(QWidget):
         icon_path = "C:\\Users\\HPCar\\Documents\\Sistema_Contable\\Sistema_Contable (Grafico)\\Interrogacion.png"
         self.setWindowIcon(QIcon(icon_path)) 
         self.apply_custom_style()
+        Texto1 = Main_Label("Contenido nuevo: ")
+        Horizontal = Posiciones(ventana)
+        Horizontal.addWidget(Texto1)
     def apply_custom_style(self):
-        QStyle.children()
-        
-                
-        
+        self.setStyleSheet('background-color: lightblue;')  
+ventana = MainWindow()     
 app = QApplication(sys.argv)
-ventana = MainWindow()
 ventana.show()
-app.exec_()    
+app.setStyle("Fusion")
+app.exec_() 
+   
     

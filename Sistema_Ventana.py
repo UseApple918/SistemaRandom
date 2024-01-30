b14c82e8680c99d3f0b9fcb0072177f97fb05729
UseApple918 <136140544+UseApple918@users.noreply.github.com>
2024-01-30 14:31:44 -0500

    Segundo Commit

--- Sistema_Ventana.py
+++ Sistema_Ventana.py
@@ -1,32 +1,36 @@
 import sys
 from typing import Optional
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QWidget , QPushButton , QLabel, QVBoxLayout
-global ventana
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
 class Main_Label(QLabel):
     def __init__(self, texto):
         super().__init__(texto)
 
-class Posiciones (QVBoxLayout):
+class Posiciones(QVBoxLayout):
     def __init__(self, Label):
-        super().__init__(Label)
-          
+        super().__init__()
+        self.addWidget(Label)
+
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Numero Aleatorio")
         icon_path = "C:\\Users\\HPCar\\Documents\\Sistema_Contable\\Sistema_Contable (Grafico)\\Interrogacion.png"
-        self.setWindowIcon(QIcon(icon_path)) 
+        self.setWindowIcon(QIcon(icon_path))
         self.apply_custom_style()
         Texto1 = Main_Label("Contenido nuevo: ")
-        Horizontal = Posiciones(ventana)
-        Horizontal.addWidget(Texto1)
+        Horizontal = Posiciones(Texto1)
+        self.setLayout(Horizontal)  # Establecer el layout de la ventana principal
+
     def apply_custom_style(self):
-        self.setStyleSheet('background-color: lightblue;')  
-ventana = MainWindow()     
+        self.setStyleSheet('background-color: lightblue;')
+
 app = QApplication(sys.argv)
+ventana = MainWindow()
 ventana.show()
 app.setStyle("Fusion")
-app.exec_() 
+app.exec_()
+
    
     
